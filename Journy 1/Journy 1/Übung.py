4fb02ca269a57d12fdbd2609fbaade1ad4cb6187
xTyndx <bamberg.patrick98@web.de>
2025-04-02 10:47:25 +0200

    init

---
+++ Journy 1/Journy 1/Übung.py
@@ -0,0 +1,18 @@
+name = "Maja"
+age = 2
+
+name1 = "Luna"
+age1 = 2
+
+name2 = "Patrick"
+age2 = 26
+
+name3 = "Jessy"
+age3= 27
+
+#durchschnitt
+print((age + age1 + age2 + age3) / 4)
+avarage_age = ((age + age1 + age2 + age3) / 4)
+
+print("Hallo " + name + ", " + name1 + ", " + name2 + " und " + name3 + 
+      "! Euer Durchschnittsalter liegt bei " + str(avarage_age))
