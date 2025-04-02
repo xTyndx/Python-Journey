07f10404953451d137902b71ea92aa0ad5d88518
xTyndx <bamberg.patrick98@web.de>
2025-04-02 10:36:39 +0200

    init

---
+++ Journy 1/Journy 1/basics.py
@@ -0,0 +1,25 @@
+#variables
+
+'''a = (5 + 6) 
+b = (5 * 2)
+c = (5 / 2)
+
+print((a * c) + b * a)'''
+
+#durchschnitt
+
+'''age = 20
+age2 = 30
+
+print((age + age2) / 2)
+
+avarage_age = ((age + age2) / 2)
+print(avarage_age)'''
+
+#strings
+
+'''name = "Patrick"
+print("Ich bin: " + name)
+
+age = 26
+print("Ich bin: " + str(age) + " Jahre alt")'''
