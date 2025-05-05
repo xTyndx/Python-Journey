6a751cdc4cfc088bdd65cdb20b968fffadd12604
xTyndx <bamberg.patrick98@web.de>
2025-05-05 09:35:56 +0200

    init

---
+++ Journy 1/Journy 1/Funktionen/Datein.py
@@ -0,0 +1,8 @@
+# file = open("schreiben.txt", "w")
+# students = ["Max", "Moritz", "Hans", "Peter"]
+# for student in students:
+#     file.write(student + "\n")
+# file.close()
+
+# With
+
---
+++ Journy 1/Journy 1/Funktionen/Funktionen.py
@@ -0,0 +1,46 @@
+# def multi_print():
+#     print("Hallo Welt")
+#     print("Hallo Welt")
+
+# multi_print():
+
+# def multi_print2(name):
+#     print(name)
+#     print(name)
+
+# multi_print2("Hallo Welt")
+
+
+# def multi_print3(name, count):
+#     for i in range(count):
+#         print(name)
+
+# multi_print3("Hallo Welt", 5)
+
+# def weitere_funktion():
+#     multi_print3("Hallo Welt", 5)
+#     multi_print3("Welt", 3)
+
+
+# weitere_funktion()
+
+# RETURN
+
+# def maximum(a, b):
+#     if a > b:
+#         return a
+#     else:
+#         return b
+    
+# result = maximum(5, 10)
+# print(result)  # Gibt 10 aus
+
+
+# Objekte
+
+# liste = [1, 2, 3]
+# print(liste)
+
+# liste.append(4)
+# print(liste)  # Gibt [1, 2, 3, 4] aus
+
